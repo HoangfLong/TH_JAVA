fa8eda92bae22514cb78a34713d47bb40d16d951
Hoàng Long Vjp Pro.No1 <150314384+HoangfLong@users.noreply.github.com>
2024-06-07 13:07:36 +0700

    push first pj

---
+++ TH_buoi3/src/main/java/com/example/TH_buoi3/controller/BookController.java
@@ -0,0 +1,44 @@
+package com.example.TH_buoi3.controller;
+
+import com.example.TH_buoi3.entity.Book;
+import com.example.TH_buoi3.services.BookService;
+import com.example.TH_buoi3.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/books")
+public class BookController {
+
+    @Autowired
+    private BookService bookService;
+
+    private CategoryService categoryService;
+
+    @GetMapping
+    public String showAllBooks(Model model) {
+        List<Book> books = bookService.getAllBooks();
+        model.addAttribute("books",books);
+        return "book/list";
+    }
+    //Thêm 1 cuốn sách
+    @GetMapping("/add")
+    public String addBookForm(Model model) {
+        model.addAttribute("book",new Book());
+        model.addAttribute("categories",categoryService.getAllCategories());
+        return "book/add";
+    }
+
+    @PostMapping("/add")
+    public String addBookForm(@ModelAttribute("book") Book book) {
+        bookService.addBook(book);
+        return "redirect:/books";
+    }
+}
